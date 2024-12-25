1a6b4909d1795f08025a8ca66cf608d8cc442f32
kingslydev <ghalyallcoc@gmail.com>
2024-12-25 23:27:29 +0700

    initials transactiontype tabel revisi

--- models/transactionType.go
+++ models/transactionType.go
@@ -1,13 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
-
-type TransactionType struct{
-	gorm.Model
-	Name string `json:"name" gorm:"size:255; not null"`
-	Code string `json:"code" gorm:"size:255;unique;not null"`
-	Action string `json:"action" gorm:"type:enum('cr', 'dr'); not null"`
-	Thumbnail string `json:"thumbnail" gorm:"size:255; not null"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+type TransactionType struct {
+    gorm.Model
+    Name      string         `json:"name" gorm:"size:255;not null"`
+    Code      string         `json:"code" gorm:"size:255;unique;not null"`
+    Action    string         `json:"action" gorm:"type:action_type;not null"`  // Menggunakan tipe enum yang sudah ada
+    Thumbnail string         `json:"thumbnail" gorm:"size:255;not null"`
+    DeletedAt gorm.DeletedAt `json:"deleted_at"`
 }
